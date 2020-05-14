4eccb935306f4bf0f04a6469d8837a59bcdab653
NoahHeadley <noahkito@gmail.com>
2020-05-14 15:06:51 -0400

    Added comments to vote.py

--- vote.py
+++ vote.py
@@ -1,3 +1,8 @@
+"""
+This program will create a Maximum Spanning Tree connecting each of the eyes based on the clusterings given.
+It then prunes the weakest connections until the eyes are all in their final calculated clusters.
+The eyes are moved into folders with arbitrary numbers that represent that cluster.
+"""
 import os
 import argparse
 import sys
@@ -29,7 +34,7 @@ while (cluster_num < sys.maxsize):
 
 
 cluster_files = []
-
+# Gather each of the clusterings provided
 for file in os.listdir(src):
     if(file.endswith('_vga_sym_XBSym_mod')):
         cluster_files.append(file)
@@ -37,9 +42,13 @@ for file in os.listdir(src):
 # A Buddyship indicates if a pair of eyes are in the same clique (i.e. cluster)
 num_buddys = len(os.listdir(dest))
 num_cliques = len(cluster_files)
+
+# 3D boolean table
 buddyships = np.zeros((num_cliques, num_buddys, num_buddys))
 
 np.set_printoptions(threshold=sys.maxsize)
+
+# Read each of the cluster files and place them into cliques. A clique is one of the clusters in the clustering group.
 
 for f_index in range(num_cliques):
     clique_file = open(cluster_files[f_index], 'r')
@@ -47,7 +56,8 @@ for f_index in range(num_cliques):
     lines.pop(0)
 
     clique_nums = []
-
+    
+    # Find out which clique this eye is in and place it in a list
     for line in lines:
         line = line.strip().split('   ')
 
@@ -56,6 +66,7 @@ for f_index in range(num_cliques):
         if(clique_val.isdecimal()):
             clique_nums.append(clique_val)
 
+    # Buddyships will contain a 1 if the two eyes being compared are in a clique together and 0 otherwise
     for y_buddy in range(num_buddys):
         for x_buddy in range(y_buddy, num_buddys):
             if(clique_nums[y_buddy] == clique_nums[x_buddy]):
@@ -63,6 +74,7 @@ for f_index in range(num_cliques):
 
 flat = []
 
+# Flatten the 3D matrix into a 2D one by summing up the z-entries
 for y in range(len(buddyships[0])):
     temp = np.zeros(len(buddyships[0]))
     for x in range(len(buddyships)):
@@ -70,6 +82,8 @@ for y in range(len(buddyships[0])):
     temp = list(temp)
     flat.append(temp)
 
+
+# Make a graph showing all the points in space
 
 graph = {
     'vertices': [x for x in range(num_buddys)],
@@ -81,12 +95,17 @@ for y in range(num_buddys):
         if(flat[y][x] > 0):
             graph['edges'].add((flat[y][x], y, x))
 
+# Apply Kruskal's algorithm for a Max Spanning Tree
 mst = mk.kruskal(graph)
+
+# Do k-pruning
 s_mst = []
 for l in range(len(mst)):
     if(l > len(mst)):
         break
     (w, x, y) = mst[l]
+    
+    # weight is being hard coded but this gives the desired output for now.
     if (w > 5):
         s_mst.append(mst[l])
 
@@ -99,6 +118,8 @@ else:
         (w, x, y) = line
         f.write(f"{x},{y},{w}\n")
 
+# # This code when uncommented will show the graph formed
+
 # G = nx.read_edgelist("./s_mst.txt", delimiter=',', data=[('weight', float)])
 # G.edges(data=True)
 
@@ -109,6 +130,8 @@ else:
 
 # plt.show()
 
+
+# Sort the points by their parent in the edge-graph
 s_mst.sort(key=lambda y: y[1])
 
 out_pts = []
@@ -118,25 +141,12 @@ for x in range(len(s_mst)):
     out_pts.append(p_out)
     in_pts.append(p_in)
 
-# for x in range(num_buddys):
-#     while out_pts.count(x) > 1:
-#         y = out_pts.index(x)
-#         in_pts.pop(y)
-#         out_pts.remove(x)
-
-clusters = []
-# idea: add every point that connects to this point by checking every point connected to it in in and out pts
-# make it be a list where each entry is the connection. If the pt exists in any of the lists found then just ignore
-
-
 G = connection.Graph(num_buddys)
 for x in range(len(out_pts)):
     G.addEdge(out_pts[x], in_pts[x])
 
+# Get all points that are connected to each other    
 cc = G.connectedComponents()
-
-for line in cc:
-    print(line)
 
 clusters = []
 for x in range(num_buddys):
@@ -144,6 +154,7 @@ for x in range(num_buddys):
         if x in cc[y]:
             clusters.append(y)
 
+# Move files to their final cluster
 source = './all/'
 files = os.listdir(source)
 
